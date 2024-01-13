5a70c9e750eddb20b0b78bb5ae0fe30bca28621c
Gyuzal <aidarkhanova.g.03@mail.ru>
2024-01-13 15:57:37 +0100

    .

--- src/pages/HomePage/components/Profile/Profile.js
+++ src/pages/HomePage/components/Profile/Profile.js
@@ -8,7 +8,7 @@ function Profile() {
                     <h1>Hi, I am John,<br/> Creative Technologist</h1>
                     <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
                 </div>
-                <img src="./media/john.png" alt="john" className={s.profileImg}/>
+                <img src="./john.png" alt="john" className={s.profileImg}/>
             </div>
             <a href="https://cdn-images.livecareer.co.uk/pages/free_cv_templates_lcuk_11.jpg" download className={s.download_link}>Download Resume</a>
         </div>
